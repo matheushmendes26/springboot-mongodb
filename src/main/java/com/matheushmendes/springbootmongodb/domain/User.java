5c8e7cc5d5eca24404dcda067ee99abc0c14ae7e
Matheus Mendes <matheush.1998@hotmail.com>
2023-04-07 15:42:19 -0300

    Connect to MongoDB with repository and service

--- src/main/java/com/matheushmendes/springbootmongodb/domain/User.java
+++ src/main/java/com/matheushmendes/springbootmongodb/domain/User.java
@@ -3,17 +3,21 @@ package com.matheushmendes.springbootmongodb.domain;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 
 @Getter
 @Setter
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Document(collection = "user")
 public class User implements Serializable {
     //It works to convert the objects in bites for transport in ner or files;
     private static final long serialVersionUID = 1L;
 
     @EqualsAndHashCode.Include
+    @Id
     private String id;
     private String name;
     private String email;
--- src/main/java/com/matheushmendes/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/matheushmendes/springbootmongodb/resources/UserResource.java
@@ -1,6 +1,8 @@
 package com.matheushmendes.springbootmongodb.resources;
 
 import com.matheushmendes.springbootmongodb.domain.User;
+import com.matheushmendes.springbootmongodb.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,13 +16,15 @@ import java.util.List;
 @RestController //Identify a resource rest
 @RequestMapping(value = "/users") //Set the route of the resource
 public class UserResource {
+
+    @Autowired
+    private UserService userService;
+
     //alternative way to mapping and set method http is @GetMapping for method Get
     @RequestMapping(method = RequestMethod.GET)//to set endpoint way for the rout "/users"
     public ResponseEntity<List<User>> findAll(){
-        User maria = new User("1","Maria Silva", "maria@gmail.com");
-        User daniel = new User("2","Daniel White", "daniel@gmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria,daniel));
+
+        List<User> list = userService.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
